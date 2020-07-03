e1748a16ee3672b3751a867c50fa6b07cec184aa
Cendeal <1798062051@qq.com>
2020-07-04 02:30:02 +0800

    update

--- script.sql
+++ script.sql
@@ -27,13 +27,13 @@ create table if not exists score
     score      int(3)     null
 );
 
-INSERT INTO student_examination_sys.student (id, name, age, sex) VALUES ('001', 'zhangsan', 18, 'male');
-INSERT INTO student_examination_sys.student (id, name, age, sex) VALUES ('002', 'lisi', 20, 'female');
+INSERT INTO student (id, name, age, sex) VALUES ('001', '张三', 18, '男');
+INSERT INTO student (id, name, age, sex) VALUES ('002', '李四', 20, '女');
 
-INSERT INTO student_examination_sys.subject (id, subject, teacher, description) VALUES ('1001', 'Chinese', 'Mr.Wang', 'the exam is easy');
-INSERT INTO student_examination_sys.subject (id, subject, teacher, description) VALUES ('1002', 'math', 'Miss Liu', 'the exam is difficult');
+INSERT INTO subject (id, subject, teacher, description) VALUES ('1001', '语文', '王老师', '本次考试比较简单');
+INSERT INTO subject (id, subject, teacher, description) VALUES ('1002', '数学', '刘老师', '本次考试比较难');
 
-INSERT INTO student_examination_sys.score (id, student_id, subject_id, score) VALUES (1, '001', '1001', 80);
-INSERT INTO student_examination_sys.score (id, student_id, subject_id, score) VALUES (2, '002', '1002', 60);
-INSERT INTO student_examination_sys.score (id, student_id, subject_id, score) VALUES (3, '001', '1001', 70);
-INSERT INTO student_examination_sys.score (id, student_id, subject_id, score) VALUES (4, '002', '1002', 60);
+INSERT INTO score (student_id, subject_id, score) VALUES ('001', '1001', 80);
+INSERT INTO score (student_id, subject_id, score) VALUES ('002', '1002', 60);
+INSERT INTO score (student_id, subject_id, score) VALUES ('001', '1001', 70);
+INSERT INTO score (student_id, subject_id, score) VALUES ('002', '1002', 60);
